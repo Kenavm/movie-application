4b30afa5e2ad1a7d50a58fc0c21abfc150b71b80
Manuel <manuel.vanek@gmail.com>
2023-03-18 20:26:26 +0100

    added migrateDb and model Film

---
+++ backend/model/Film.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const filmSchema = mongoose.Schema({
+    _id: {
+      type: mongoose.Schema.Types.ObjectId,
+    },
+    title: String,
+    plot: String,
+    genres: Array,
+    runtime: Number,
+    year: Number,
+    imdb: {
+      rating: Number,
+      votes: Number,
+      id: String,
+    },
+    poster: String,
+  });
+
+
+const Film = movieConnection.model("Movie", filmSchema, "films");
+
+export default Film
