bceb42ae7af0f04abc9da497cae991e88ab8e54c
Manuel <manuel.vanek@gmail.com>
2023-03-20 14:23:46 +0100

    fixed cinemaConnection

--- backend/config/cinemaConnection.js
+++ backend/config/cinemaConnection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import colors from "colors";
 
 const cinemaConnection = async () => {
-	const conn = mongoose.createConnection(process.env.DB_CINEMA_URL, {
+	const conn = mongoose.connect(process.env.DB_CINEMA_URL, {
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
 	});
